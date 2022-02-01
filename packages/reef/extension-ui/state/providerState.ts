9b989df074c01c1594e79dabd50e149720091d09
Matjaz Hirsman <matjazonline@gmail.com>
2022-02-01 23:44:40 +0100

    website

--- packages/reef/extension-ui/state/providerState.ts
+++ packages/reef/extension-ui/state/providerState.ts
@@ -4,12 +4,12 @@ import {Network, Networks, utils} from '@reef-defi/react-lib';
 
 export const providerSubj = new ReplaySubject<Provider>(1);
 export const networksSubj = new BehaviorSubject<Networks>(utils.availableReefNetworks);
-export const selectedNetworkSubj = new BehaviorSubject<Network>(utils.availableReefNetworks.mainnet);
-/*export const selectedNetworkSubj = new BehaviorSubject<Network>({
+// export const selectedNetworkSubj = new BehaviorSubject<Network>(utils.availableReefNetworks.mainnet);
+export const selectedNetworkSubj = new BehaviorSubject<Network>({
   name: 'testnet',
   rpcUrl: 'ws://localhost:9944',
   reefscanUrl: 'http://localhost:8000',
   factoryAddress: '0xcA36bA38f2776184242d3652b17bA4A77842707e',
   routerAddress: '0x0A2906130B1EcBffbE1Edb63D5417002956dFd41',
-});*/
+});
 selectedNetworkSubj.subscribe((network) => console.log('NETWORK=', network.rpcUrl));
